c6c0712a1c758cbd10381d195970f7d2c0dd1620
iyatomi takehiro <iyatomi@gmail.com>
2012-12-07 06:54:01 +0900

    investigate test hungup

--- test/lua/exit.lua
+++ test/lua/exit.lua
@@ -3,11 +3,16 @@ local yue = require('_inc')
 
 print('-- test yue exit -----------------------------------------')
 local ok, r = yue.client(function(cl)
+	print('start client')
 	local c = yue.open('tcp://localhost:8888').procs
+	print('call rpc')
 	c.async_keepalive('hogehoge'):on(function(ok,r)
+		print('receive response:', ok, r)
 		assert(ok and (r == 'hogehoge'))
 		cl:exit(true, "test exit")
 		assert(false) -- should not reach here
 	end)
 end)
+print('end yue.client:', ok, r)
 assert(r == "test exit")
+print('pass assert')
