82fb6fd1b49b934089bc7bcd1d4aaf9f7196f9e6
iyatomi takehiro <iyatomi@gmail.com>
2013-01-11 22:57:02 +0900

    change luarock installation script so that it does not use popd, pushd (because travis env does not support these)

--- src/ll/lua/exlib/config.luarocks.sh
+++ src/ll/lua/exlib/config.luarocks.sh
@@ -7,13 +7,14 @@ fi
 
 echo "luarocks $1"
 
-pushd $1
+PWD=`pwd`
+cd $1
 
 ./configure --with-lua=/usr/local --prefix=/usr/local --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0
 
 make ; make install
 
-popd
+cd $PWD
 
 
 
