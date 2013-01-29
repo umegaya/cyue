e773efbc2064a4bb044cfe2026b9b353863a4fd6
Takehiro Iyatomi <iyatomi@gmail.com>
2013-01-29 16:15:20 +0900

    fix compilation error on android client build

--- src/jni/ll/lua/Android.mk
+++ src/jni/ll/lua/Android.mk
@@ -8,9 +8,21 @@ $(call ndk_log,$(LIB_SRC_PATH))
 NDK = $(shell dirname `which ndk-build`)
 NDKABI = 8
 NDKVER = arm-linux-androideabi-4.6
+HOST_ARCH = $(shell uname)
+$(call ndk_log,Host Arch: $(HOST_ARCH))
+ifeq ($(HOST_ARCH), Darwin)
+	NDK_HOST_ARCH = 'darwin-x86'
+else
+	ifeq ($(HOST_ARCH), Win32)
+		# TODO : we can know win32 and what to do for handling it?
+		NDK_HOST_ARCH = 'win32??'
+	else
+		NDK_HOST_ARCH = 'linux-x86'
+	endif
+endif
 define build_luajit_arm
-	$(call ndk_log,build luajit arm using NDK @ $(NDK) $(NDKABI) $(NDKVER))
-	$(shell bash $(LOCAL_PATH)/build_luajit_arm.sh $(LIB_SRC_PATH) $(NDK) $(NDKABI) $(NDKVER) $(MY_LUAJIT_OPTIONAL_CFLAGS))
+	$(call ndk_log,build luajit arm using NDK @ $(NDK) $(NDKABI) $(NDKVER) $(NDK_HOST_ARCH) $(LIB_SRC_PATH))
+	$(shell bash $(LOCAL_PATH)/build_luajit_arm.sh $(LIB_SRC_PATH) $(NDK) $(NDKABI) $(NDKVER) $(NDK_HOST_ARCH) $(MY_LUAJIT_OPTIONAL_CFLAGS))
 endef
 
 $(call ndk_log,$(call build_luajit_arm))
--- src/jni/ll/lua/build_luajit_arm.sh
+++ src/jni/ll/lua/build_luajit_arm.sh
@@ -1,14 +1,14 @@
 NDK=$2
 NDKABI=$3
 NDKVER=$NDK/toolchains/$4
-NDKP=$NDKVER/prebuilt/linux-x86/bin/arm-linux-androideabi-
-NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm $5"
+NDKP=$NDKVER/prebuilt/$5/bin/arm-linux-androideabi-
+NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm $6"
 CURRENTDIR=`dirname $0`
 pushd $1
 #make clean
 MAKE="make HOST_CC=\"gcc -m32\" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS=\"$NDKF\""
 >&2 printf "build luajit: $MAKE\n"
-if [ $# -gt 4 ]; then
+if [ $# -gt 5 ]; then
 	echo $MAKE | sh 2> /dev/null
 else
 	echo $MAKE | sh
